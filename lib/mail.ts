817ae87e7136e94a5948aed8dfaefd4c83c2d79d
Shivam <shivamrai6836@gmail.com>
2024-01-08 14:51:55 +0530

    email domain

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,6 +1,7 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY)
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorEmail = async(
     email : string,
@@ -25,7 +26,7 @@ export const sendVerificationEmail = async (
     email : string,
     token : string
 ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
     await resend.emails.send({
         from : "onboarding@resend.dev",
         to : email,
@@ -41,7 +42,7 @@ export const sendPaaswordResetEmail = async (
     email: string,
     token: string
 ) => {
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+    const resetLink = `${domain}/auth/new-password?token=${token}`;
     await resend.emails.send({
         from : "onboarding@resend.dev",
         to : email,
